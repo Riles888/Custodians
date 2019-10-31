346439d44f4c82319d767e36395b504c9bffe7e7
Bohannan Bees <beesmb@miamioh.edu>
2019-10-31 16:38:45 -0400

    services Page

---
+++ Property/servicesPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Property
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class servicesPage : TabbedPage
+    {
+        public servicesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
